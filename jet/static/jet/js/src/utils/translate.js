b363b4139c352f682b2b3c06051244c0b96c930c
Niels Van Och <niels.vanoch@gmail.com>
2020-10-16 11:35:20 +0200

    Make compatible with Django 3
    Fix all wrong imports and references to `django.gettext`.

--- jet/static/jet/js/src/utils/translate.js
+++ jet/static/jet/js/src/utils/translate.js
@@ -1,6 +1,6 @@
 module.exports = function(str) {
-    if (window.django == undefined) {
+    if (window.gettext == undefined) {
         return str;
     }
-    return django.gettext(str);
+    return window.gettext(str);
 };
